a5509e7c149bf23606184ef8d1b41786870a11d8
Anubhav-Tomar <122211486+Anubhav-Tomar@users.noreply.github.com>
2024-09-06 18:15:38 +0530

    Time: 18 ms (43.49%), Space: 10 MB (32.69%) - LeetHub

--- 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.cpp
@@ -2,33 +2,26 @@ class Solution {
 public:
     int mod = 1e9 + 7;
 
-    int solve(int dice, int faces, int target, vector<vector<int> > &dp){
-        if(target < 0){
-            return 0;
-        }
-        if(target == 0 && dice != 0){
-            return 0;
-        }
-        if(target != 0 && dice == 0){
-            return 0;
-        }
-        if(target == 0 && dice == 0){
-            return 1;
-        }
+    int solve(int d, int f, int t){
+        vector<vector<int> > dp(d+1, vector<int>(t+1, 0));
+        
+        dp[0][0] = 1;
 
-        if(dp[dice][target] != -1){
-            return dp[dice][target];
+        for(int dice = 1; dice <= d; dice++){
+            for(int target = 1; target <= t; target++){
+                int ans = 0;
+                for(int i=1; i<=f; i++){
+                    if( target - i >= 0){
+                        ans = (ans + dp[dice-1][target-i]) % mod;
+                    }
+                }
+                dp[dice][target] = ans;
+            }
         }
-
-        int ans = 0;
-        for(int i=1; i<=faces; i++){
-            ans = (ans + solve(dice-1, faces, target-i, dp) )% mod;
-        }
-        return dp[dice][target] = ans;
+        return dp[d][t];
     }
 
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int> > dp(n+1, vector<int>(target+1, -1));
-        return solve(n, k, target, dp);
+        return solve(n, k, target);
     }
 };
