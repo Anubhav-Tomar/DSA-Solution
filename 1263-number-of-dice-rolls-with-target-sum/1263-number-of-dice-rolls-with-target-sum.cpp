c3d7c16d44105ede4a814ed1249bdf30ae47d507
Anubhav-Tomar <122211486+Anubhav-Tomar@users.noreply.github.com>
2024-09-06 18:20:01 +0530

    Time: 20 ms (34.8%), Space: 7.9 MB (90.32%) - LeetHub

--- 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.cpp
@@ -3,22 +3,24 @@ public:
     int mod = 1e9 + 7;
 
     int solve(int d, int f, int t){
-        vector<vector<int> > dp(d+1, vector<int>(t+1, 0));
-        
-        dp[0][0] = 1;
+    vector<int> prev(t+1, 0);
+    vector<int> curr(t+1, 0);
 
-        for(int dice = 1; dice <= d; dice++){
-            for(int target = 1; target <= t; target++){
-                int ans = 0;
-                for(int i=1; i<=f; i++){
-                    if( target - i >= 0){
-                        ans = (ans + dp[dice-1][target-i]) % mod;
-                    }
+    prev[0] = 1;
+
+    for(int dice = 1; dice <= d; dice++){
+        for(int target = 1; target <= t; target++){
+            int ans = 0;
+            for(int i=1; i<=f; i++){
+                if( target - i >= 0){
+                    ans = (ans + prev[target-i]) % mod;
                 }
-                dp[dice][target] = ans;
             }
+            curr[target] = ans;
+            }
+            prev = curr;
         }
-        return dp[d][t];
+        return prev[t];
     }
 
     int numRollsToTarget(int n, int k, int target) {
