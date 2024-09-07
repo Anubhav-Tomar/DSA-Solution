8538748895bb49eca3d08365546c029814edccc6
Anubhav-Tomar <122211486+Anubhav-Tomar@users.noreply.github.com>
2024-09-07 13:49:37 +0530

    Time: 113 ms (92.67%), Space: 93.1 MB (92.53%) - LeetHub

--- 0819-minimum-swaps-to-make-sequences-increasing/0819-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0819-minimum-swaps-to-make-sequences-increasing/0819-minimum-swaps-to-make-sequences-increasing.cpp
@@ -2,7 +2,11 @@ class Solution {
 public:
     int solve(vector<int>& nums1, vector<int>& nums2){
         int n = nums1.size();
-        vector<vector<int> > dp(n+1, vector<int>(2, 0));
+        
+        int swap = 0;
+        int noswap = 0;
+        int currswap = 0;
+        int currnoswap = 0;
 
         for(int index = n-1; index >= 1; index--){
             for(int swapped = 1; swapped >=0; swapped--){
@@ -12,20 +16,30 @@ public:
                 int ans = INT_MAX;
 
                 if(swapped){
-                    swap(prev1, prev2);
+                    int temp = prev1;
+                    prev1 = prev2;
+                    prev2 = temp;
                 }
                 if(nums1[index] > prev1 && nums2[index] > prev2){
-                    ans = dp[index+1][0];
+                    ans = noswap;
                 }
 
                 if(nums1[index] > prev2 && nums2[index] > prev1){
-                    ans = min(ans, (1 + dp[index+1][1]) );
+                    ans = min(ans, (1 + swap) );
                 }
 
-                dp[index][swapped] = ans;
+                if(swapped){
+                    currswap = ans;
+                }
+                else{
+                    currnoswap = ans;
+                }
+                
             }
+            swap = currswap;
+            noswap = currnoswap;
         }
-        return dp[1][0];
+        return min(currswap, currnoswap);
     }
 
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
