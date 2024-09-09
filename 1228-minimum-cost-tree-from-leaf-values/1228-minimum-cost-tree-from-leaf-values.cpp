fd521915f4bf0f682be6b5fb9e054d0e01a8452b
Anubhav-Tomar <122211486+Anubhav-Tomar@users.noreply.github.com>
2024-09-09 11:39:43 +0530

    Time: 30 ms (51.52%), Space: 14.8 MB (45.77%) - LeetHub

--- 1228-minimum-cost-tree-from-leaf-values/1228-minimum-cost-tree-from-leaf-values.cpp
+++ 1228-minimum-cost-tree-from-leaf-values/1228-minimum-cost-tree-from-leaf-values.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    int solve(vector<int>& arr, map<pair<int, int>, int> &maxi, int left, int right, vector<vector<int> > &dp){
-        if(left == right){
-            return 0;
-        }
+    int solve(vector<int>& arr, map<pair<int, int>, int> &maxi){
+        int n = arr.size();
+        vector<vector<int> > dp(n, vector<int>(n, 0));
 
-        if(dp[left][right] != -1){
-            return dp[left][right];
-        }
+        for(int left=n-1; left>=0; left--){
+            for(int right=0; right<n; right++){
+                if(left >= right){
+                    continue;
+                }
+                else{
+                    int ans = INT_MAX;
 
-        int ans = INT_MAX;
-
-        for(int i=left; i<right; i++){
-            ans = min(ans, maxi[{left, i}]*maxi[{i+1, right}] + solve(arr, maxi, left, i, dp) + solve(arr, maxi, i+1, right, dp));
+                    for(int i=left; i<right; i++){
+                        ans = min(ans, maxi[{left, i}]*maxi[{i+1, right}] +dp[left][i] + dp[i+1][right]);
+                    }
+                    dp[left][right] = ans;
+                }
+            }
         }
-        return dp[left][right] = ans;
+        return dp[0][n-1];
     }
 
     int mctFromLeafValues(vector<int>& arr) {
-        int n = arr.size();
-        vector<vector<int> > dp(n+1, vector<int>(n+1, -1));
         map<pair<int, int>, int> maxi;
+        int n = arr.size();
 
         for(int i=0; i<n; i++){
             maxi[{i,i}] = arr[i];
@@ -28,6 +32,6 @@ public:
                 maxi[{i, j}] = max(arr[j], maxi[{i, j-1}]);
             }
         }
-        return solve(arr, maxi, 0, n-1, dp);
+        return solve(arr, maxi);
     }
 };
