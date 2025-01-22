8e034ba160aae4efacfb540fe8f95b6fb68911ad
Anubhav-Tomar <122211486+Anubhav-Tomar@users.noreply.github.com>
2025-01-22 12:40:17 +0530

    Time: 11 ms (26.9%), Space: 20.1 MB (43.32%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+
+        for(auto i: prerequisites) {
+            int u = i[1];
+            int v = i[0];
+            adj[u].push_back(v);
+        }
+
+        vector<int> indegree(numCourses, 0);
+        for(int i=0; i<numCourses; i++){
+            for(auto j: adj[i]){
+                indegree[j]++;
+            }
+        }
+
+        queue<int> q;
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        vector<int> ans;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto i: adj[node]){
+                indegree[i]--;
+                if(indegree[i] == 0){
+                    q.push(i);
+                }
+            }
+        }
+
+        if(ans.size() == numCourses){
+            return true;
+        }
+
+        return false;
+    }
+};
