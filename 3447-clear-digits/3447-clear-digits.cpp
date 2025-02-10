3ab46a2844b0a0a0985d6b41805fdcb2cd911fed
Anubhav-Tomar <122211486+Anubhav-Tomar@users.noreply.github.com>
2025-02-10 08:06:14 +0530

    Time: 0 ms (100%), Space: 8.3 MB (87.14%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        int n = s.size();
+        string ans = "";
+
+        for(char i: s){
+            if(i >= '0' && i <= '9'){
+                if(!ans.empty()){
+                    ans.pop_back();
+                }
+            }
+            else{
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
